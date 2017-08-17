7ffdbe36fad65938d6c6fdf265054c6a88505a50
Terra Tateyama <ttateyama@mastercontrol.com>
2017-08-16 22:22:27 -0600

    Finished PROBLEM 7

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -97,9 +97,14 @@ alert(newMyName());
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-
+function outerFn() {
+	return function() {
+		return "Terra Tateyama";
+	}
+}
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+var innerFn = outerFn();
 //Now invoke innerFn.
+innerFn();
